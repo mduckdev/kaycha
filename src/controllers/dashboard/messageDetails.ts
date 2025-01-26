f8ac4d71e4fc0c21ab4807c50f29f6b48d54fd71
mduckdev <mduckdev@gmail.com>
2025-01-26 12:55:17 +0100

    Message details updated

--- src/controllers/dashboard/messageDetails.ts
+++ src/controllers/dashboard/messageDetails.ts
@@ -2,11 +2,22 @@
 import { Request, Response } from 'express';
 import { AppDataSource } from '../../data-source';
 import { Message } from '../../entity/Message';
+import { TransportMessage } from '../../entity/TransportMessages';
 
 export const messageDetailsController = async (req: Request, res: Response) => {
     const messageId = req.params.id;
+    const src = req.query.src;
+    let messageRepository;
 
-    const messageRepository = (await AppDataSource).getRepository(Message);
+    if(src==="kaczormaszyny.pl"){
+        messageRepository = (await AppDataSource).getRepository(Message);
+    }
+    if(src==="kaczortransport.pl"){
+        messageRepository = (await AppDataSource).getRepository(TransportMessage);
+    }
+    if(!messageRepository){
+        return res.status(404).send('Wiadomość o podanym ID nie została znaleziona.');
+    }
     try {
         const messageDetails = await messageRepository.findOneByOrFail({ id: Number(messageId) });
         res.render('message-details', { message: messageDetails  });
