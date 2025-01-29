074f39c389925cc37c1d59af82081144320accd8
mduckdev <mduckdev@gmail.com>
2025-01-29 16:39:39 +0100

    dist deleted

--- dist/controllers/dashboard/profile.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.profileController = void 0;
-const profileController = (req, res) => {
-    res.render("profile", { user: req.session.user, csrfToken: req.session.csrfToken });
-};
-exports.profileController = profileController;
